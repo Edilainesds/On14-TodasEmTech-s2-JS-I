10cdb4bf3ac7f6aa5cda2c6788f5f89489229458
Edilainesds <edilaine45@yahoo.com.br>
2021-08-28 18:46:03 -0300

    atividade reprograma

---
+++ Reprograma atividades/Exemploaula2.js
@@ -0,0 +1,15 @@
+let mensagem = 'SEJA BEM VINDO AO MEU SITE';
+alert(mensagem);
+
+let num1 = 86;
+let num2 = 97;
+let soma = num1 + num2;
+alert(soma);
+
+let num3 = 5;
+let num4 = 8;
+let multiplicacao = num3 * num4;
+alert(multiplicacao);
+
+let listademercado = ['maca' , 'uva', 'abacate' , 'manga' , 'banana' , 'maca'];
+alert(listademercado);
